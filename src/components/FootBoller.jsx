7e5d976111e060249e26660a6e62532ced72693a
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-06-17 19:46:30 +0600

    button slice click

--- src/components/FootBoller.jsx
+++ src/components/FootBoller.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Singleplayer from './singlePlayers/Singleplayer';
 const FootBoller = () => {
   const [players,setPlayers] = useState([])
+  const[open,setOpen] = useState(false)
 
 
   useEffect(()=>{
@@ -9,15 +10,26 @@ const FootBoller = () => {
      .then(res=>res.json())
      .then(data=>setPlayers(data))
   },[])
+
+  const handleDataShow=()=>{
+    console.log('click me')
+    setOpen(true)
+  }
   
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:px-4' >
+    <>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:px-4 mb-6' >
      {
-      players.map(player=> <Singleplayer
+       players.slice(0,open?9:6).map(player=> <Singleplayer
       key={player.id}
       player={player}></Singleplayer>)
      }
+    {
+      !open &&
+      <button onClick={()=>handleDataShow(true)} className='bg-orange-400 hover:bg-red-700 px-4 py-2 w-28 rounded mx-auto'>See mor</button>
+    }
     </div>
+    </>
   );
 };
 
